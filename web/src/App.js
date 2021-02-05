8de44e5c6e20a63d0783d7ca23162c2d982143e5
Tyler Piccolo <trpic@outlook.com>
2021-02-04 21:45:29 -0600

    react tutorial

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './Components/NavigationBar/NavigationBar';
+import SignIn from './Components/SignIn'
+import Greet from './Components/Greet'
+import Welcome from './Components/Welcome'
+import Hello from './Components/Hello'
 
 function App() {
   return (
     <div className="App">
       <NavigationBar/>
+      <Greet name="Tyler" heroName="Superman"> <p> This is children props</p></Greet>
+      <Greet name="Diana" heroName="Joker"> 
+        <button>Dynamic data that i may not know what it will be, here</button>
+      </Greet>
+      <Greet name="Bruce" heroName="Joker"/>
+      <Welcome name="Tyler" heroName="Superman"> </Welcome>
+      <Welcome name="Tyler" heroName="Superman"> </Welcome>
+      <Hello/>
+      <SignIn/>
     </div>
   );
 }
