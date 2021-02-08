628a4df7dc5f9e3fb26ff4c24e5bc38436ab369e
Tyler Piccolo <trpic@outlook.com>
2021-02-08 14:51:17 -0600

    Update App.js, Home.js, and NavigationBar.css

--- web/src/App.js
+++ web/src/App.js
@@ -6,7 +6,7 @@ import { Route, Switch } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home/Home.js';
 import Login from './Components/LogIn/Login.js';
-<<<<<<< HEAD
+
 import VideoPlayback from './Components/video/VideoPlayback.js'
 
 function App() {
@@ -21,29 +21,7 @@ function App() {
         </Switch>
       </BrowserRouter>
     </div>
-=======
-import Members from './Components/Members/Members.js';
-import Events from './Components/Events/Events.js';
-import About from './Components/About/About.js';
-import Join from './Components/Join/Join.js';
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <NavigationBar/>
-      <Switch>
-        <Route exact path='/home' component={Home}/>
-        <Route exact path='/events' component={Events}/>
-        <Route exact path='/about' component={About}/>
-        <Route exact path='/join' component={Join}/>
-        <Route exact path='/login' component={Login}/>
-        <Route exact path='/members' component={Members}/>
-        
-      </Switch>
-    </BrowserRouter>
     
->>>>>>> 65042d32914d03d2b1b7c98be36403d335bd8a49
   );
 }
 
