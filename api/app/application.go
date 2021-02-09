04aeae140f38b7e3420afb9a130d2b5016b27e03
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-02-08 21:38:16 -0600

    implement general controllers, buisness and db logic for blogs

--- api/app/application.go
+++ api/app/application.go
@@ -1,9 +1,12 @@
 package app
 
 import (
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lindseypoche/SELU_ACM/api/clients/mongo"
 )
 
 var (
@@ -12,12 +15,27 @@ var (
 
 func getPort() string {
 	if os.Getenv("PORT") != "" {
-		return os.Getenv("PORT")
+		return ":" + os.Getenv("PORT")
 	}
 	return ":8080"
 }
 
+// StartApplication ...
 func StartApplication() {
+
+	mongo.Init()
+
 	mapUrls()
-	router.Run(":" + getPort())
+
+	srv := &http.Server{
+		Addr:         "127.0.0.1" + getPort(),
+		Handler:      router,
+		WriteTimeout: 500 * time.Millisecond,
+		ReadTimeout:  2 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
--- api/app/url_mappings.go
+++ api/app/url_mappings.go
@@ -1,8 +1,19 @@
 package app
 
-import "github.com/lindseypoche/SELU_ACM/api/controllers/ping"
+import (
+	"github.com/lindseypoche/SELU_ACM/api/controllers"
+)
 
 func mapUrls() {
 
-	router.GET("/ping", ping.Ping)
+	router.GET("/ping", controllers.PingController.Ping)
+
+	// blog endpoints
+	router.POST("/blogs", controllers.BlogsController.Create)
+	router.GET("/blogs/:blog_id", controllers.BlogsController.Get)
+	router.GET("/blogs", controllers.BlogsController.GetAll)
+
+	// user endpoints
+	// router.POST("/user", controllers.UsersController.Create)
+	// router.GET("/user:user_id", controllers.UsersController.Get)
 }
