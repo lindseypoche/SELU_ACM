068900d6fc49e9f3cb0a4048b69f6fab5b55f1a6
Tyler Piccolo <trpic@outlook.com>
2021-02-10 16:02:40 -0600

    Update Home.js

--- web/src/Components/Home/Home.js
+++ web/src/Components/Home/Home.js
@@ -1,32 +1,22 @@
-import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import './Home.css';
-import HomeCaro from './HomeCaro.js';
-import ParallaxContainer from './ParallaxContainer';
-import discord from '../image/Discord.png';
-
-
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import "./Home.css";
+import HomeCaro from "./HomeCaro.js";
+import ParallaxContainer from "./ParallaxContainer";
+import discord from "../image/Discord.png";
 
 class Home extends Component {
-
   render() {
     return (
-
-      
       <div>
-<<<<<<< HEAD
-        <div className="discord-thing"><a href="https://discord.gg/g6bQXFMjs3"><img src={discord}/></a>
-        </div>      
-      <ParallaxContainer/>
+        <div className="discord-thing">
+          <a href="https://discord.gg/g6bQXFMjs3">
+            <img src={discord} />
+          </a>
+        </div>
+        <ParallaxContainer />
       </div>
-=======
-        <div className="discord-thing"><a href="https://discord.gg/g6bQXFMjs3"><img src={discord}/></a></div>      
-        <ParallaxContainer/>
-       </div>
->>>>>>> Dev
-
-
-    )
+    );
   }
 }
 
