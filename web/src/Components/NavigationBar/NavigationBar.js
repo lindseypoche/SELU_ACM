66ce1e4a86a0e04e53764a06aad388ca60c0e88c
Tyler Piccolo <trpic@outlook.com>
2021-03-04 16:11:04 -0600

    resize navbar

--- web/src/Components/NavigationBar/NavigationBar.js
+++ web/src/Components/NavigationBar/NavigationBar.js
@@ -49,7 +49,7 @@ const NavigationBar = (props) => {
               <NavLink className="acm" tag={Link} to="/calendar">Calendar</NavLink>
             </NavItem>
           </Nav>
-          <Nav className="rightSide" navbar>
+          {/* <Nav className="rightSide" navbar>
           <UncontrolledDropdown nav inNavbar>
               <DropdownToggle className="acm" nav caret>
                 Admin
@@ -73,7 +73,7 @@ const NavigationBar = (props) => {
               <NavItem>
                 <NavLink className="acm" tag={Link} to="/login">Login</NavLink>
               </NavItem>
-            </Nav>
+            </Nav> */}
         </Collapse>
       </Navbar>
     </div>
