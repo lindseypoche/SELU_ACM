840c319be89abd70a4eb44d8f5cd0ec7bcb321f7
Lindsey Poche <lindsey.poche-2@selu.edu>
2021-03-04 16:27:14 -0600

    officer junk

--- web/src/Components/OfficerCard/OfficerCard.js
+++ web/src/Components/OfficerCard/OfficerCard.js
@@ -4,21 +4,36 @@ import {
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 import './OfficerCard.css';
-import Pao from '../image/ACM Officers/pao.resize.jpg'
+import './Box.css'
+
+
 
 const OfficerCard = (props) => {
+  const cardInfo = [
+    {image: "", title: "Kuo-Pao Yang", subtitle: "Faculty Advisor", text: "Kuo-pao Yang is a professor in the Department of Computer Science at Southeastern Louisiana University.He received his B.S. degree in Computer Science at Tamkang University, Taipei, Taiwan, R.O.C. He earned his M.S. and his Ph.D. degree in Computer Science at Illinois Institute of Technology. His research interests include Computer Architecture, Programming Languages, and Expert Systems."},
+    {image: "", title: "Lauren Pace", subtitle: "President", text: ""},
+    {image: "",title: "Kevin Ziebarth", subtitle: "Vice-President", text: ""},
+    {image: "",title: "Craig Canepa", subtitle: "Secretary", text: "Craig Canepa is a junior Information Technology major. He is currently a tutor with the Computer Science Department. After receiving his Bachelors degree, he hopes to get a job. Outside of academics, his interests are playing video games, watching movies, and playing with his pets."},
+    {image: "",title: "Lindsey Poche", subtitle: "Treasurer", text: "Lindsey Poche is a Senior Computer Science Major.She is currently a tutor with the Computer Science Department as Southeastern Lousisiana University. Along with being the Treasurer for the Association for Computing Machinery, she is also the Treasurer for American Welding Society at Southeastern Louisiana University. Outside of school she enjoys swimming and knitting."},
+    ];
+  
+  const renderCard = (card, index) => {
+    return (
+      <Card style = {{width: "18rem "}} key = {index} className= "box">
+      <CardImg variant = "top" src={card.image}/>
+      <CardBody>
+        
+        <CardTitle tag="h5">{card.title}</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">{card.subtitle}</CardSubtitle>
+        <CardText>{card.text}</CardText>
+        <Button>Read More</Button>
+      </CardBody>
+    </Card>
+    )
+  };
   return (
-    <div>
-      <Card>
-        <CardImg variant = "top" src={Pao} title="Placeholder" />
-        <CardBody>
-          <CardTitle tag="h5">Officer Name</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Officer Title</CardSubtitle>
-          <CardText>Blurb</CardText>
-          <Button>Read More</Button>
-        </CardBody>
-      </Card>
-      
+    <div className = "grid">
+        {cardInfo.map(renderCard)}    
     </div>
   );
 };
--- web/src/Components/Resources/Resources.js
+++ web/src/Components/Resources/Resources.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './Resources.css';
+import Fay from '../image/Schedules/FAY125Schedule.png'
+import CSTB from '../image/Schedules/CSTB2026Schedule.png'
 
 
 
@@ -10,7 +12,20 @@ class Resources extends Component {
     return (
       <div className="resourcesPage"> 
         <h1>Resources</h1>
-
+        <center>
+        <br></br>
+        <h3>CSTB 2026 Schedule</h3>
+        <div className = "image">
+        <img src={CSTB} alt ="CSTB Schedule"  ></img>
+        </div>
+        <br></br>
+        
+        <h3>Fayard 125 Schedule</h3>
+        <div className = "image">
+        <img src={Fay} alt ="Fay Schedule"></img>
+        </div>
+        </center>
+        <br></br>
       </div>
     )
   }
