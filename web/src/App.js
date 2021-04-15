8a8918c7be212602acdb22daf39768a2f9b77401
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-04-15 18:40:06 -0500

    fe changes

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,7 @@ import Calendar from './Pages/Calendar/CalendarApp.js';
 import Officers from './Pages/Officers/Officers.js'
 import EventsPage from './Pages/EventsPage/EventsPage.js';
 import Resources from './Pages/Resources/Resources.js';
-// import Footer from './Unused Components/Footer/Footer.js';
+import Footer from './Components/Footer/Footer.js';
 import SingleEventPage from './Pages/SingleEventPage/SingleEventPage.js';
 import ScrollToTop from './Components/BackTop/BackTop.js';
 
@@ -19,7 +19,7 @@ function App() {
       <BrowserRouter>
         <NavigationBar/>
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path='/home' component={Home} />
           <Route exact path='/events' component={EventsPage} />
           <Route exact path='/calendar' component={Calendar} />
           <Route exact path='/officers' component={Officers} />
