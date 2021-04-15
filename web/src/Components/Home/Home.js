0c404bf0f8c9a12c19f0a85916da50aa77beed8d
Austin Benton <austin@aebenton.com>
2021-04-14 19:16:51 -0500

    Reorganized Web and Responsive Navbar

---
+++ web/src/Components/Home/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import "./Home.css";
+
+import ParallaxContainer from "./ParallaxContainer";
+import discord from "../image/Discord.png";
+import VideoPlayback from '../video/VideoPlayback.js'
+
+
+class Home extends Component {
+  render() {
+    return (
+      
+      <div className="homePage">
+
+        <VideoPlayback />
+        <div className="discord-thing">
+          <a href="https://discord.gg/g6bQXFMjs3">
+            <img src={discord} />
+          </a>
+        </div>
+        <ParallaxContainer />
+        
+      </div>
+    );
+  }
+}
+
+export default Home;
