f8817f6f838d4e42bae9c9dea907702c1cf5d68f
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-04-16 01:04:11 -0500

    create package for working with time

---
+++ web/src/Utils/timing.js
@@ -0,0 +1,53 @@
+// utility functions for time
+
+export const toDateFormat = (unix) => {
+  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+  var d = new Date(unix * 1000);
+  var month = months[d.getMonth()];
+  var f = month + " " + d.getDay().toString()
+  return f
+}
+
+export const isExpiring = (exp_date) => {
+  var seconds_remaining = parseInt(exp_date) - Date.now()/ 1000
+  if(seconds_remaining < 0) {
+    return false
+  }
+  // if event is between 0 and 2 days 
+  if(seconds_remaining < 86400*2) {
+    return true
+  }
+  return false
+}
+
+export const getRemainingTime = (start_date) => {
+  var format = ""
+  var delta = parseInt(start_date) - Date.now()/ 1000
+
+  if(delta < 0) {
+    return "ended"
+  }
+  if(delta < 3600) {
+    return "starts in < 1 hour"
+  }
+
+  // calculate and subtract days
+  var days = Math.floor(delta / 86400); 
+  delta -= days * 86400;
+  if(days < 2) {
+    format = "starts in " + days + " day"
+  } else {
+    format = "starts in " + days + " days"
+  }
+
+  // calculate and subtract hours 
+  var hours = Math.floor(delta / 3600) % 24;
+  delta -= hours * 3600;
+  if(hours < 2) {
+    format += " and " + hours + " hour"
+  } else {
+    format += " and " + hours + " hours"
+  }
+
+  return format
+}
