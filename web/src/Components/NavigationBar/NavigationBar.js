5d053d1300eda89780abad0ef76406f7ae1d3be8
tylerpiccolo <tyler.piccolo@selu.edu>
2021-04-16 19:48:49 -0500

    nav links now have hot reload without page refresh

--- web/src/Components/NavigationBar/NavigationBar.js
+++ web/src/Components/NavigationBar/NavigationBar.js
@@ -5,26 +5,28 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useHistory
 } from "react-router-dom";
 import './NavigationBar.css';
 
 const NavigationBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  let history = useHistory();
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div className="navacmbg">
       < Navbar className="navacm" collapseOnSelect expand="md" bg="dark" fixed="top" variant="dark" >
-        <Navbar.Brand className="acm" href="/home"><img style={{ height: '50px', width: '50px', marginRight: '10px' }} src={acmlogo} />Association for Computing Machinery</Navbar.Brand>
+        <Navbar.Brand className="acm" onClick={()=> history.push("/home")}><img style={{ height: '50px', width: '50px', marginRight: '10px' }} src={acmlogo} />Association for Computing Machinery</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link className="acm" href="/officers">Officers</Nav.Link>
-            <Nav.Link className="acm" href="/events">Events</Nav.Link>
-            <Nav.Link className="acm" href="/resources">Resources</Nav.Link>
-            <Nav.Link className="acm" href="/calendar">Calendar</Nav.Link>
+            <Nav.Link className="acm" onClick={()=> history.push("/officers")}>Officers</Nav.Link>
+            <Nav.Link className="acm" onClick={()=> history.push("/events")}>Events</Nav.Link>
+            <Nav.Link className="acm" onClick={()=> history.push("/resources")}>Resources</Nav.Link>
+            <Nav.Link className="acm" onClick={()=> history.push("/calendar")}>Calendar</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar >
