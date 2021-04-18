dafcd12b50cf9eca3500197fb0ae381c64bc4ab3
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-04-18 02:18:05 -0500

    implement service layer for creating, editing, and deleting comments

--- api/internal/commenting/service.go
+++ api/internal/commenting/service.go
@@ -1,1 +1,65 @@
 package commenting
+
+import (
+	"github.com/cmd-ctrl-q/SELU_ACM/api/internal/utils/date_utils"
+	"github.com/cmd-ctrl-q/SELU_ACM/api/internal/utils/errors/rest"
+)
+
+var err rest.Err
+
+// Repository provides access to the comment storage.
+type Repository interface {
+	SaveComment(*Comment) rest.Err
+	EditComment(*Comment) rest.Err
+	DeleteComment(string) rest.Err
+}
+
+// Service provides comment listing operations.
+type Service interface {
+	AddComment(*Comment) rest.Err
+	EditComment(*Comment) rest.Err
+	DeleteComment(string) rest.Err
+}
+
+type service struct {
+	r Repository
+}
+
+// NewMessageService creates a new message service
+func NewService(repo Repository) Service {
+	return &service{r: repo}
+}
+
+// AddComment adds a comment by the message id
+func (s *service) AddComment(comment *Comment) rest.Err {
+
+	err = s.r.SaveComment(comment)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// EditComment ...
+func (s *service) EditComment(comment *Comment) rest.Err {
+	comment.EditedTimestamp = date_utils.GetNowUnix()
+
+	// edit comment in mongo db
+	err = s.r.EditComment(comment)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteComment ...
+func (s *service) DeleteComment(id string) rest.Err {
+	// delete comment from mongo db
+	err = s.r.DeleteComment(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
