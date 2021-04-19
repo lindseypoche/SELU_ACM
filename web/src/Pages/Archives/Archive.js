dd98336e86a92772b5bdde31ab3b76d49ab6cde3
tylerpiccolo <tyler.piccolo@selu.edu>
2021-04-18 21:57:10 -0500

    file Archive named right. file path proble is fix

---
+++ web/src/Pages/Archives/Archive.js
@@ -0,0 +1,37 @@
+// import React, {useState, useEffect} from "react"
+// import axios from "axios";
+// import { Link } from "react-router-dom";
+
+const Archive = () => {
+
+    
+
+    // const [archives, setArchives] = useState([]);
+    // const [archivesIsLoaded, setArchivesIsLoaded] = useState(false)
+
+    // useEffect(() => {
+    //     getArchives();
+    // }, [])
+
+    // const getEvents = () => {
+    //    axios.get(`http://localhost:8081/api/archives/${match.params.id}`)
+    //        .then(((response) => {  
+    //            setArchives(response.data);
+    //            setArchivesIsLoaded(true);
+    //       }))
+    //       .catch(error => console.log(error))
+    //   }
+
+    //   if (!eventsIsLoaded) {
+    //     return <div className="App">Loading...</div>;
+    //   }
+
+    //   console.log("archives: ")
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Archive;
