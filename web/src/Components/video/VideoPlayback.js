12ea93d134934bd06618543413bc3e5b73fdf697
Lindsey Poche <lindsey.poche-2@selu.edu>
2021-05-05 18:30:43 -0500

    finished membership page

--- web/src/Components/video/VideoPlayback.js
+++ web/src/Components/video/VideoPlayback.js
@@ -30,38 +30,3 @@ const VideoPlayback = () => {
 }
 
 export default VideoPlayback
-
-
-// var NewComponent = React.createClass({
-//     render: function() {
-//       return (
-//         <div>
-//           <header className="video-header">
-//             <video src="https://css-tricks-post-videos.s3.us-east-1.amazonaws.com/Island%20-%204141.mp4" autoPlay loop playsInline muted />
-//             <div className="viewport-header">
-//               <h1>
-//                 Explore
-//                 <span>Montana</span>
-//               </h1>
-//             </div>
-//           </header>
-//           <main>
-//             [[[https://codepen.io/chriscoyier/pen/VbqzER]]]
-//           </main>
-//           <div id="circle" />
-//         </div>
-//       );
-//     }
-//   });
-
-
-
-// var viewportHeader = document.querySelector(".viewport-header");
-
-// document.body.addEventListener("scroll", function(event) {
-//   var opacity = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
-//   var scale = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
-//   document.documentElement.style.setProperty('--headerOpacity', opacity);
-//   document.documentElement.style.setProperty('--headerScale', scale);
-// });
-
--- web/src/Pages/Membership/Membership.js
+++ web/src/Pages/Membership/Membership.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './Membership.css';
 import square from "../../Media/Images/Icons/squareUp.png";
+import form from "../../Media/Forms/SELU ACM Membership Registration Form.pdf";
 import pdf from "../../Media/Images/Icons/pdf.png";
+import acm from '../../Media/Images/Icons/association-for-computing-machinery-logo.png';
 
 
 class Membership extends Component {
@@ -13,23 +15,41 @@ class Membership extends Component {
           <h1>Membership</h1>
           <br></br>
           <br></br>
-          <div className="form">
-            <p>import forms and then have acm email to send forms to</p>
-
-            <div className="download">
-              <a href="web\src\Media\Forms\SELU ACM Membership Registration Form.pdf">
-                <img src={pdf}/>
-              </a>
-            </div>
-            <br></br>
+          <div className = "acm">
+            <img src = {acm}></img>
           </div>
-          <div className="square">
-            <a href="https://checkout.square.site/merchant/1VRKAABSW92ZE/checkout/I2XGX7WFRSNVVTXCC4HTHDXE">
-            <center><img src={square} alt="SquareImg"  ></img></center>
+          <br></br>
+          <br></br>
+          <h3>Want to Join the ACM?</h3>
+          <br></br>
+          <p1>Download these forms and either bring them to a local ACM Officer or email them to us at:</p1>
+          <a href="mailto:acm@selu.edu"> acm@selu.edu</a>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div className = "forms">
+            <a href = {form}>
+              <img src={pdf} alt = "pdf"></img>
+            </a>
+          </div>
+          <div className = "form1">
+            <a src = {pdf}>
+            <p>SELU ACM Membership Form</p>
             </a>
           </div>
           <br></br>
-          <p>add paypal??</p>    
+          <br></br>
+          <p>Then pay membership fees via Square or in person with a local SELU ACM Officer.</p>
+          <br></br>
+          <br></br>
+          <div className="square">
+            <a href="https://checkout.square.site/merchant/1VRKAABSW92ZE/checkout/I2XGX7WFRSNVVTXCC4HTHDXE">
+              <img src={square} alt="SquareImg"  ></img>
+            </a>
+          </div>
+          <br></br>
+   
         </div>
       </div>
     )
